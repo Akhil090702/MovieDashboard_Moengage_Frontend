4f6c3f76eb1773d0256a5f1259cde507089a25d7
Akhil Goyal <108260649+Akhil090702@users.noreply.github.com>
2025-09-03 18:05:22 +0530

    Base URL modified

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:2000/movies', // backend route
+  baseURL: 'https://moviedashboardbackend.vercel.app/movies', // backend route
 });
 
 export default api;
