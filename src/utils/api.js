d090c04acb19c20e04b37b19646c9888e4ebfc81
Akhil Goyal <108260649+Akhil090702@users.noreply.github.com>
2025-09-04 16:18:01 +0530

    initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://moviedashboardbackend01.vercel.app/movies',
+  baseURL: 'https://moviedashboard-moengage-backend.onrender.com/movies',
 });
 
 
